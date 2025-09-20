f93ed02c9a2cfb6fb739f0facfaa8a59591139dc
KamilKonopski <Kamil.Konopski.2000@gmail.com>
2025-09-20 18:23:25 +0200

    add main app component with step-based navigation between cover and card screens

--- App.tsx
+++ App.tsx
@@ -1,20 +1,28 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { View } from "react-native";
+import { CoverScreen } from "./src/screens/CoverScreen";
+import { CardScreen } from "./src/screens/CardScreen";
+import { proposals } from "./src/data/dateCards";
+import {
+  useFonts,
+  DancingScript_400Regular,
+} from "@expo-google-fonts/dancing-script";
 
 export default function App() {
+  const [step, setStep] = useState<"cover" | "cards">("cover");
+
+  const [] = useFonts({
+    DancingScript_400Regular,
+  });
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1 }}>
+      {step === "cover" && <CoverScreen onStart={() => setStep("cards")} />}
+      {step === "cards" && (
+        <>
+          <CardScreen data={proposals} goBack={() => setStep("cover")} />
+        </>
+      )}
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
